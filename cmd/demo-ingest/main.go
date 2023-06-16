afb0cb21995bfdb6accdc5ce1e35197246041e08
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2023-06-15 22:26:32 -0600

    Add comprehensive comments the demo CLI
    This commit adds comments to the demo CLI in cmd/demo-ingest/main.go
    with some instructions on how to use it. Note: This file is temporary
    while we have tests and a CLI.
    
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

--- cmd/demo-ingest/main.go
+++ cmd/demo-ingest/main.go
@@ -15,6 +15,12 @@ import (
 
 var filename = filepath.Join(os.TempDir(), "sbom.proto")
 
+// NOTICE:
+// This program is a demo to test the protobom functions while
+// we have a proper CLI and tests. It can be run directly.
+// It expects an SBOM as its first argument and it will perform
+// some tricks based on the following lines. See comments below
+
 func main() {
 	if len(os.Args) != 2 {
 		logrus.Fatalf("usage: %s sbom.json", os.Args[0])
@@ -23,21 +29,31 @@ func main() {
 	var doc *sbom.Document
 
 	parser := reader.New()
-
 	doc, err := parser.ParseFile(os.Args[1])
 	if err != nil {
 		logrus.Fatalf("parsing file: %v", err)
 	}
 
+	// Uncomment this to see a dump of the protobom go struct
 	// fmt.Printf("%+v", doc)
 
+	// Uncomment the following line to write the raw protobuf output
+	// to the path in `filename` (defined above):
 	// writeProto(doc)
+
+	// If you have the raw protobuf written in `filename` (defined above)
+	// you can re-read the doc variable from its contents by uncommenting
+	// the following line:
 	// doc = readProto()
 
+	// Create a new renderer
 	renderer := writer.New()
 
+	// The SBOM read will be rewritten to the following format:
+	// renderer.Options.Format = formats.CDX14JSON
 	renderer.Options.Format = formats.SPDX23JSON
 
+	// Serialize and render the protobom to STDOUT:
 	if err := renderer.WriteStream(doc, os.Stdout); err != nil {
 		logrus.Fatalf("writing sbom to stdout: %v", err)
 	}
@@ -46,11 +62,11 @@ func main() {
 func writeProto(bom *sbom.Document) {
 	out, err := proto.Marshal(bom)
 	if err != nil {
-		logrus.Fatal("marshalling sbom to protobuf: %v", err)
+		logrus.Fatalf("marshalling sbom to protobuf: %v", err)
 	}
 
 	if err := os.WriteFile(filename, out, os.FileMode(0o644)); err != nil {
-		logrus.Fatal("writing data to disk: %v", err)
+		logrus.Fatalf("writing data to disk: %v", err)
 	}
 }
 

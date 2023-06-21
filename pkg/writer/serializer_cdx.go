800bccb3faeaa142adbf102b60ef0929e21ecdd2
Mikey Strauss <mdstrauss91@gmail.com>
2023-06-21 14:00:49 +0300

    thread safe serial
    Signed-off-by: Mikey Strauss <mdstrauss91@gmail.com>

--- pkg/writer/serializer_cdx.go
+++ pkg/writer/serializer_cdx.go
@@ -1,6 +1,7 @@
 package writer
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
@@ -10,21 +11,20 @@ import (
 	cdx "github.com/CycloneDX/cyclonedx-go"
 	"github.com/bom-squad/protobom/pkg/sbom"
 	"github.com/bom-squad/protobom/pkg/writer/options"
+	uuid "github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 )
 
-type SerializerCDX struct {
-	rootDict       map[string]struct{}
-	addedDict      map[string]struct{}
-	componentsDict map[string]*cdx.Component
-	reflessList    []*cdx.Component
-}
+const (
+	PROTOBOM_REF_PREFIX = "__protobom_auto_noref"
+)
+
+type SerializerCDX struct{}
 
 func (s *SerializerCDX) Serialize(opts options.Options, bom *sbom.Document) (interface{}, error) {
-	s.rootDict = map[string]struct{}{}
-	s.addedDict = map[string]struct{}{}
-	s.componentsDict = map[string]*cdx.Component{}
-	s.reflessList = []*cdx.Component{}
+	state := NewSerializerCDXState()
+	ctx := context.Background() //2DO Should we include a context in the interface ?
+	ctx = WithCDXState(ctx, state)
 
 	doc := cdx.NewBOM()
 	doc.SerialNumber = bom.Metadata.Id
@@ -42,50 +42,28 @@ func (s *SerializerCDX) Serialize(opts options.Options, bom *sbom.Document) (int
 	doc.Components = &[]cdx.Component{}
 	doc.Dependencies = &[]cdx.Dependency{}
 
-	root, err := s.root(bom)
-	// 2DO Just warning until resolve BUG - the root spdx node is not found in the node list (search by ID)
-	if err != nil {
-		logrus.Warnf("Root missing")
-		// return nil, err
-	} else {
-		doc.Metadata.Component = root
-	}
+	doc.Metadata.Component = s.root(ctx, bom)
 
-	err = s.componentsMaps(bom)
+	err = s.componentsMaps(ctx, bom)
 	if err != nil {
 		return nil, err
 	}
 
-	deps, err := s.dependencies(bom)
+	deps, err := s.dependencies(ctx, bom)
 	if err != nil {
 		return nil, err
 	}
-
 	doc.Dependencies = &deps
-	components := s.components()
+
+	components := state.components()
 	doc.Components = &components
 
 	return doc, nil
 }
 
-func (s *SerializerCDX) components() []cdx.Component {
-	var components []cdx.Component
-	for _, c := range s.componentsDict {
-		if _, ok := s.addedDict[c.BOMRef]; ok {
-			continue
-		}
-		components = append(components, *c)
-	}
+func (s *SerializerCDX) componentsMaps(ctx context.Context, bom *sbom.Document) error {
+	state, _ := GetCDXState(ctx) // 2DO what should happen when no state is found?
 
-	// Add components without refs
-	for _, c := range s.reflessList {
-		components = append(components, *c)
-	}
-
-	return components
-}
-
-func (s *SerializerCDX) componentsMaps(bom *sbom.Document) error {
 	for _, n := range bom.Nodes {
 		comp := s.nodeToComponent(n)
 		if comp == nil {
@@ -94,28 +72,32 @@ func (s *SerializerCDX) componentsMaps(bom *sbom.Document) error {
 		}
 
 		if comp.BOMRef == "" {
-			s.reflessList = append(s.reflessList, comp)
-		} else {
-			s.componentsDict[comp.BOMRef] = comp
+			comp.BOMRef = s.generateRef()
 		}
+
+		state.componentsDict[comp.BOMRef] = comp
 	}
 	return nil
 }
 
-func (s *SerializerCDX) root(bom *sbom.Document) (*cdx.Component, error) {
-	var rootComp *cdx.Component
-	found := false
+// 2DO FIX ME https://github.com/bom-squad/protobom/issues/23
+func (s *SerializerCDX) generateRef() string {
+	return fmt.Sprintf("%s-%s", PROTOBOM_REF_PREFIX, uuid.New())
+}
 
+func (s *SerializerCDX) root(ctx context.Context, bom *sbom.Document) *cdx.Component {
+	var rootComp *cdx.Component
 	// First, assign the top level nodes
+	state, _ := GetCDXState(ctx) // 2DO what should happen when no state is found?
+
+	// 2DO Use GetRootNodes() https://github.com/bom-squad/protobom/pull/20
 	if bom.RootElements != nil && len(bom.RootElements) > 0 {
 		for _, id := range bom.RootElements {
-			s.rootDict[id] = struct{}{}
 			// Search for the node and add it
 			for _, n := range bom.Nodes {
 				if n.Id == id {
 					rootComp = s.nodeToComponent(n)
-					found = true
-					s.addedDict[id] = struct{}{}
+					state.addedDict[id] = struct{}{}
 				}
 			}
 
@@ -125,24 +107,21 @@ func (s *SerializerCDX) root(bom *sbom.Document) (*cdx.Component, error) {
 		}
 	}
 
-	if !found {
-		return nil, fmt.Errorf("no root component")
-	}
-
-	return rootComp, nil
+	return rootComp
 }
 
 // NOTE dependencies function modifies the components dictionary
-func (s *SerializerCDX) dependencies(bom *sbom.Document) ([]cdx.Dependency, error) {
+func (s *SerializerCDX) dependencies(ctx context.Context, bom *sbom.Document) ([]cdx.Dependency, error) {
 
 	var dependencies []cdx.Dependency
+	state, _ := GetCDXState(ctx) // 2DO what should happen when no state is found?
 
 	for _, e := range bom.Edges {
-		if _, ok := s.addedDict[e.From]; ok {
+		if _, ok := state.addedDict[e.From]; ok {
 			continue
 		}
 
-		if _, ok := s.componentsDict[e.From]; !ok {
+		if _, ok := state.componentsDict[e.From]; !ok {
 			logrus.Info("serialize")
 			return nil, fmt.Errorf("unable to find component %s", e.From)
 		}
@@ -154,22 +133,22 @@ func (s *SerializerCDX) dependencies(bom *sbom.Document) ([]cdx.Dependency, erro
 		case sbom.Edge_contains:
 			// Make sure we have the target component
 			for _, targetID := range e.To {
-				s.addedDict[targetID] = struct{}{}
-				if _, ok := s.componentsDict[targetID]; !ok {
+				state.addedDict[targetID] = struct{}{}
+				if _, ok := state.componentsDict[targetID]; !ok {
 					return nil, fmt.Errorf("unable to locate node %s", targetID)
 				}
 
-				if s.componentsDict[e.From].Components == nil {
-					s.componentsDict[e.From].Components = &[]cdx.Component{}
+				if state.componentsDict[e.From].Components == nil {
+					state.componentsDict[e.From].Components = &[]cdx.Component{}
 				}
-				*s.componentsDict[e.From].Components = append(*s.componentsDict[e.From].Components, *s.componentsDict[targetID])
+				*state.componentsDict[e.From].Components = append(*state.componentsDict[e.From].Components, *state.componentsDict[targetID])
 			}
 
 		case sbom.Edge_dependsOn:
 			// Add to the dependency tree
 			for _, targetID := range e.To {
-				s.addedDict[targetID] = struct{}{}
-				if _, ok := s.componentsDict[targetID]; !ok {
+				state.addedDict[targetID] = struct{}{}
+				if _, ok := state.componentsDict[targetID]; !ok {
 					return nil, fmt.Errorf("unable to locate node %s", targetID)
 				}
 
@@ -278,4 +257,37 @@ func (s *SerializerCDX) renderVersion(cdxVersion cdx.SpecVersion, doc interface{
 	}
 
 	return nil
+}
+
+type SerializerCDXState struct {
+	addedDict      map[string]struct{}
+	componentsDict map[string]*cdx.Component
+}
+
+func NewSerializerCDXState() *SerializerCDXState {
+	return &SerializerCDXState{
+		addedDict:      map[string]struct{}{},
+		componentsDict: map[string]*cdx.Component{},
+	}
+}
+
+func (s *SerializerCDXState) components() []cdx.Component {
+	var components []cdx.Component
+	for _, c := range s.componentsDict {
+		if _, ok := s.addedDict[c.BOMRef]; ok {
+			continue
+		}
+		components = append(components, *c)
+	}
+
+	return components
+}
+
+func WithCDXState(ctx context.Context, state *SerializerCDXState) context.Context {
+	return context.WithValue(ctx, "state", state)
+}
+
+func GetCDXState(ctx context.Context) (*SerializerCDXState, bool) {
+	dm, ok := ctx.Value("state").(*SerializerCDXState)
+	return dm, ok
 }

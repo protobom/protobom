06c6ead7874945d21d3fabe230abd3431f2e981e
Adolfo Garcia Veytia (puerco) <puerco@chainguard.dev>
2023-06-26 09:32:13 -0600

    Import format sniffer from onesbom
    This commit imports the onesbom sniffer to use in protobom. It is a library
    that detects SBOM formats.
    
    Signed-off-by: Adolfo Garcia Veytia (puerco) <puerco@chainguard.dev>

---
+++ pkg/formats/sniffer.go
@@ -0,0 +1,96 @@
+package formats
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+type Sniffer struct{}
+
+// SniffFile takes a path an return the format
+func (fs *Sniffer) SniffFile(path string) (Format, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return "", fmt.Errorf("opening path: %w", err)
+	}
+	return fs.SniffReader(f)
+}
+
+// SniffReader reads a stream and return the SBOM format
+func (fs *Sniffer) SniffReader(f io.ReadSeeker) (Format, error) {
+	defer f.Seek(0, 0)
+	fileScanner := bufio.NewScanner(f)
+	fileScanner.Split(bufio.ScanLines)
+
+	formatType := ""
+	formatEncoding := ""
+	formatVersion := ""
+
+	for fileScanner.Scan() {
+		if strings.Contains(fileScanner.Text(), `"bomFormat"`) && strings.Contains(fileScanner.Text(), `"CycloneDX"`) {
+			formatType = "application/vnd.cyclonedx"
+			formatEncoding = JSON
+		}
+
+		if strings.Contains(fileScanner.Text(), `"specVersion"`) {
+			parts := strings.Split(fileScanner.Text(), ":")
+			if len(parts) == 2 {
+				ver := strings.TrimPrefix(strings.TrimSuffix(strings.TrimSuffix(strings.TrimSpace(parts[1]), ","), "\""), "\"")
+				if ver != "" {
+					formatVersion = ver
+					formatEncoding = JSON
+				}
+			}
+		}
+
+		if strings.Contains(fileScanner.Text(), "SPDXVersion:") {
+			formatType = "text/spdx"
+			formatEncoding = "text"
+
+			for _, ver := range []string{"2.2", "2.3"} {
+				if strings.Contains(fileScanner.Text(), fmt.Sprintf("SPDX-%s", ver)) {
+					formatVersion = ver
+					break
+				}
+			}
+			break
+		}
+
+		// In JSON, the SPDX version field would be quoted
+		if strings.Contains(fileScanner.Text(), "\"spdxVersion\"") ||
+			strings.Contains(fileScanner.Text(), "'spdxVersion'") {
+			formatType = "text/spdx"
+			formatEncoding = JSON
+			if formatVersion != "" {
+				break
+			}
+		}
+
+		for _, ver := range []string{"2.2", "2.3"} {
+			if strings.Contains(fileScanner.Text(), fmt.Sprintf("'SPDX-%s'", ver)) ||
+				strings.Contains(fileScanner.Text(), fmt.Sprintf("\"SPDX-%s\"", ver)) {
+				formatVersion = ver
+			}
+		}
+		if formatVersion != "" && formatType != "" && formatEncoding != "" {
+			break
+		}
+	}
+
+	fmt.Fprintf(
+		os.Stderr, "format: %s version: %s encoding: %s\n",
+		formatType, formatVersion, formatEncoding,
+	)
+
+	for _, f := range List {
+		if string(f) == fmt.Sprintf("%s+%s;version=%s", formatType, formatEncoding, formatVersion) {
+			return f, nil
+		}
+	}
+
+	// TODO(puerco): Implement a light parser in case the string hacks don't work
+	return "", fmt.Errorf("unknown SBOM format")
+}
---
+++ pkg/formats/sniffer_test.go
@@ -0,0 +1,98 @@
+package formats
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSniffReader(t *testing.T) {
+	fs := Sniffer{}
+	for _, tc := range []struct {
+		filename   string
+		mustError  bool
+		version    string
+		formatType string
+		encoding   string
+	}{
+		{
+			filename:   "testdata/linux-x64-manifest.spdx.json",
+			mustError:  false,
+			version:    "2.2",
+			formatType: "spdx",
+			encoding:   "json",
+		},
+		{
+			filename:   "testdata/nginx.spdx",
+			mustError:  false,
+			version:    "2.2",
+			formatType: "spdx",
+			encoding:   "text",
+		},
+		{
+			filename:   "testdata/nginx.spdx.json",
+			mustError:  false,
+			version:    "2.3",
+			formatType: "spdx",
+			encoding:   "json",
+		},
+		{
+			filename:   "testdata/pause.spdx",
+			mustError:  false,
+			version:    "2.3",
+			formatType: "spdx",
+			encoding:   "text",
+		},
+		{
+			filename:   "testdata/juice-shop-11.1.2.cdx.json",
+			mustError:  false,
+			version:    "1.4",
+			formatType: "cyclonedx",
+			encoding:   "json",
+		},
+		{
+			filename:  "testdata/syft.json",
+			mustError: true,
+		},
+	} {
+		f, err := os.Open(tc.filename)
+		require.NoError(t, err, tc.filename)
+
+		sbomFormat, err := fs.SniffReader(f)
+		if tc.mustError {
+			require.Error(t, err)
+			continue
+		}
+
+		require.NoError(t, err, tc.filename)
+		require.NotEmpty(t, sbomFormat)
+		require.Equal(t, tc.encoding, sbomFormat.Encoding())
+		require.Equal(t, tc.formatType, sbomFormat.Type())
+		require.Equal(t, tc.version, sbomFormat.Version())
+	}
+}
+
+func TestSniffFile(t *testing.T) {
+	fs := Sniffer{}
+	for _, tc := range []struct {
+		filename  string
+		mustError bool
+	}{
+		{
+			filename:  "testdata/linux-x64-manifest.spdx.json",
+			mustError: false,
+		},
+		{
+			filename:  "testdata/blardiboy",
+			mustError: true,
+		},
+	} {
+		_, err := fs.SniffFile(tc.filename)
+		if tc.mustError {
+			require.Error(t, err)
+		} else {
+			require.NoError(t, err)
+		}
+	}
+}

c42e8afbec5a8d62a633fa759dcd97b81f0040dc
manifestori <ori@manifestcyber.com>
2023-07-24 11:36:33 +0000

    chore: devcontainer / precommit hooks

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+PYTHON_UTILS=("yamllint" "pre-commit")
+GITHUB_UTILS=("")
+GOLANG_UTILS=("github.com/google/yamlfmt/cmd/yamlfmt@latest" "google.golang.org/protobuf/cmd/protoc-gen-go@latest")
+APT_UTILS=("shellcheck" "vim" "protobuf-compiler")
+
+set -e
+
+# Install Python tools
+if [[ $(python --version) != "" ]]; then
+    echo ====================================================
+    echo Installing Python tools...
+    export PYTHONUSERBASE=/tmp/pip-tmp
+    export PIP_CACHE_DIR=/tmp/pip-tmp/cache
+    PIPX_DIR=""
+    if ! type pipx >/dev/null 2>&1; then
+        pip3 install --disable-pip-version-check --no-cache-dir --user pipx 2>&1
+        /tmp/pip-tmp/bin/pipx install --pip-args=--no-cache-dir pipx
+        PIPX_DIR="/tmp/pip-tmp/bin/"
+    fi
+    for util in "${PYTHON_UTILS[@]}"; do
+        if ! type "${util}" >/dev/null 2>&1; then
+            "${PIPX_DIR}pipx" install --system-site-packages --pip-args '--no-cache-dir --force-reinstall' "${util}"
+        else
+            echo "${util} already installed. Skipping."
+        fi
+    done
+    rm -rf /tmp/pip-tmp
+fi
+
+# Install tools
+echo ====================================================
+echo "Installing tools from Github..."
+for util in "${GITHUB_UTILS[@]}"; do
+    if ! type "${util}" >/dev/null 2>&1; then
+        curl -s "https://raw.githubusercontent.com/${util}" | bash
+        echo ""
+    else
+        echo "${util} already installed. Skipping."
+    fi
+done
+
+# Install Golang tools
+echo ====================================================
+echo Installing Golang tools...
+for util in "${GOLANG_UTILS[@]}"; do
+    if ! type "${util}" >/dev/null 2>&1; then
+        go install "${util}"
+    else
+        echo "${util} already installed. Skipping."
+    fi
+done
+
+# Install APT tools
+echo ====================================================
+echo Installing apt tools...
+sudo apt-get update
+for util in "${APT_UTILS[@]}"; do
+    if ! type "${util}" >/dev/null 2>&1; then
+        sudo apt install -y "${util}"
+    else
+        echo "${util} already installed. Skipping."
+    fi
+done
+
+# Update .zshrc
+echo ====================================================
+echo Updating .zshrc ...
+{
+    printf "setopt appendhistory \nsetopt sharehistory \nsetopt incappendhistory \n"
+    printf "export GPG_TTY=%s\n" "$(tty)"
+} >>/home/vscode/.zshrc
+
+# Other
+echo ====================================================
+echo Finallizing ...
+pre-commit install
+pre-commit run --all-files
+
+# Done
+echo ====================================================
--- docs/regenerate-protobuf.md
+++ docs/regenerate-protobuf.md
@@ -1,10 +1,10 @@
 # Regenerate Protobuf Autogenerate Libraries
 
-The main protobom go types are generated from the 
+The main protobom go types are generated from the
 [protocol buffers definitions](api/sbom.proto). If the protobuf definitions are
 changed, the go libraries need to be regenerated.
 
-To regenerate, install the protobuf compiler `protoc`. It is available from 
+To regenerate, install the protobuf compiler `protoc`. It is available from
 the [protobuf GitHub releases page](https://github.com/protocolbuffers/protobuf/releases/latest).
 
 Once installed, simply run:
@@ -13,14 +13,13 @@ Once installed, simply run:
 protoc --go_out=pkg api/sbom.proto
 ```
 
-The main repository Makefile has a target to rebuild the libraries, you can 
+The main repository Makefile has a target to rebuild the libraries, you can
 also rebuild them by running:
 
 ```bash
 make proto
 ```
 
-After invoking the compiler, the auto generated library 
+After invoking the compiler, the auto generated library
 [`pkg/sbom/sbom.pb.go`])(pkg/sbom/sbom.pb.go) should be overwritten with the new
 version, refelcting any changes.
-

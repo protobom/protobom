d4082754a0a5e6e2e1f0bd6215ff695e32133070
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2023-08-31 21:44:20 -0600

    Conformance: Golden sample generator
    We now have a simple utility to generate golden samples for the conformance
    tests. The gerneator finds all SBOMs that correspond to a known format and
    regenerate the sample protobuf binary blobs.
    
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

---
+++ test/conformance/generator/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+
+	"github.com/bom-squad/protobom/pkg/formats"
+	"github.com/bom-squad/protobom/pkg/reader"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/protobuf/proto"
+)
+
+// This is a simple utility to generate the golden samples of the conformance
+// tests. When run, the raw protobuf equivalents of all format samples will
+// be rewritten using the latest models.
+//
+// This is designed to be run from make conformance but can be run by pointing
+// it to the testdata directory:
+//
+//	go run ./test/conformance/generator/ test/conformance/testdata
+//
+// This program should only be run when the serialization or unserialization is
+// expected to change, such as when proto files are modified.
+func main() {
+	if len(os.Args) < 2 {
+		logrus.Fatal("testdata dir not specified")
+	}
+	r := reader.New()
+	testDataDir := os.Args[1]
+	for _, format := range formats.List {
+		dataDir := filepath.Join(testDataDir, format.Type(), format.Version(), format.Encoding())
+		files, err := os.ReadDir(dataDir)
+		if err != nil {
+			if errors.Is(err, os.ErrNotExist) {
+				continue
+			}
+			logrus.Fatalf("error reading dir %s: %s", dataDir, err)
+		}
+
+		for _, f := range files {
+			if filepath.Ext(f.Name()) == ".proto" {
+				continue
+			}
+			sbomPath := filepath.Join(dataDir, f.Name())
+			sbom, err := r.ParseFile(sbomPath)
+			if err != nil {
+				logrus.Fatalf("reading sbom from %s: %s", f.Name(), err)
+			}
+			raw, err := proto.Marshal(sbom)
+			if err != nil {
+				logrus.Fatalf("marshalling sbom to protobuf: %v", err)
+			}
+
+			if err := os.WriteFile(sbomPath+".proto", raw, os.FileMode(0o644)); err != nil {
+				logrus.Fatalf("wiriting protobuf of %s: %v", f.Name(), err)
+			}
+			logrus.Infof("Wrote %s sample to %s", format, sbomPath+".proto")
+		}
+	}
+}

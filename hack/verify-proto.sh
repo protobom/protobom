08e73ca1cd9d8bc88d2754e189a50c1cb2aa4d0f
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2023-09-05 12:47:22 -0600

    Add protobuf verification presubmit
    This commit adds a new check to verify the protobuf generated
    code on every pull request.
    
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

---
+++ hack/verify-proto.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# These two are pinnedto the versions used in the latest generated code
+export PROTOC_VER="24.2" 
+export PROTOC_GO_VER="v1.31.0"
+
+curl -LO https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VER}/protoc-${PROTOC_VER}-linux-x86_64.zip
+unzip protoc-${PROTOC_VER}-linux-x86_64.zip -d $HOME/.local
+go install google.golang.org/protobuf/cmd/protoc-gen-go@${PROTOC_GO_VER}
+
+$HOME/.local/bin/protoc --proto_path=$HOME/.local/include --proto_path=. --go_out=pkg ./api/sbom.proto
+
+git diff --exit-code || echo "The protobuf definitions are not up to date. Check the docs and run make proto" && exit 1

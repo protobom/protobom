ae6492ed3ae94a46d3e83221e5b18df911cc8841
Adolfo Garcia Veytia (puerco) <puerco@chainguard.dev>
2023-10-12 12:42:36 -0800

    Update reader options struct to new pattern
    Signed-off-by: Adolfo Garcia Veytia (puerco) <puerco@chainguard.dev>

--- pkg/reader/options.go
+++ pkg/reader/options.go
@@ -1,29 +1,68 @@
 package reader
 
 import (
+	"fmt"
+
 	"github.com/bom-squad/protobom/pkg/formats"
 	"github.com/bom-squad/protobom/pkg/native"
 )
 
+type Options struct {
+	Format             formats.Format
+	UnserializeOptions *native.UnserializeOptions
+	formatOptions      map[string]interface{}
+}
+
+// argToOptsKeyVal returns a key value to access the options dictionary by using
+// key as a string or its type if its a serializer driver.
+func argToOptsKeyVal(key interface{}) string {
+	keyVal, ok := key.(string)
+	if !ok {
+		keyVal = fmt.Sprintf("%T", key)
+	}
+
+	return keyVal
+}
+
+func (o *Options) GetFormatOptions(key interface{}) interface{} {
+	keyVal := argToOptsKeyVal(key)
+	if _, ok := o.formatOptions[keyVal]; ok {
+		return o.formatOptions[keyVal]
+	}
+	return nil
+}
+
+func (o *Options) SetFormatOptions(key, opts interface{}) {
+	if o.formatOptions == nil {
+		o.formatOptions = map[string]interface{}{}
+	}
+	keyVal := argToOptsKeyVal(key)
+	if keyVal == "" {
+		return
+	}
+	o.formatOptions[keyVal] = opts
+}
+
 type ReaderOption func(*Reader)
 
-func WithUnserializeOptions(uo map[string]*native.UnserializeOptions) ReaderOption {
-	return func(w *Reader) {
+func WithFormatOptions(driverKey string, opts interface{}) ReaderOption {
+	return func(r *Reader) {
+		r.Options.SetFormatOptions(driverKey, opts)
+	}
+}
+
+func WithUnserializeOptions(uo *native.UnserializeOptions) ReaderOption {
+	return func(r *Reader) {
 		if uo != nil {
-			w.UnserializeOptions = uo
+			r.Options.UnserializeOptions = uo
 		}
 	}
 }
 
 func WithSniffer(s Sniffer) ReaderOption {
-	return func(w *Reader) {
+	return func(r *Reader) {
 		if s != nil {
-			w.sniffer = s
+			r.sniffer = s
 		}
 	}
-}
-
-type Options struct {
-	Format             formats.Format
-	UnserializeOptions *native.UnserializeOptions
 }

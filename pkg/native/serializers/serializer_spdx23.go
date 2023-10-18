508b1381100679baabc07a7dc7e5349567c36d41
Veramine <jness@veramine.com>
2023-10-18 13:23:05 -0700

    bruce forcing lint checks

--- pkg/native/serializers/serializer_spdx23.go
+++ pkg/native/serializers/serializer_spdx23.go
@@ -268,7 +268,7 @@ func buildPackages(bom *sbom.Document) ([]*spdx.Package, error) { //nolint:unpar
 			case sbom.Purpose_PLATFORM:
 				p.PrimaryPackagePurpose = "OTHER"
 			default:
-				//nolint:staticcheck // SA9003: empty branch intended for future use
+				//nolint:unused // SA9003: empty branch intended for future use
 				// TODO(degradation): Non-matching primary purpose to component type mapping
 			}
 

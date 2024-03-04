5c7184a61fd7ca21b1ab47d1261afb967f901ed5
houdini91 <mdstrauss91@gmail.com>
2024-03-04 15:47:35 +0200

    skip protoc download when already found
    Signed-off-by: houdini91 <mdstrauss91@gmail.com>

--- hack/verify-doc.sh
+++ hack/verify-doc.sh
@@ -10,10 +10,11 @@ source hack/common.sh
 export PROTOC_VER="24.4"
 export PROTOC_GEN_DOC_VER="v1.5.1"
 
-curl -LO https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VER}/protoc-${PROTOC_VER}-linux-x86_64.zip
-unzip protoc-${PROTOC_VER}-linux-x86_64.zip -d $HOME/.local
+if [ ! -f "protoc-${PROTOC_VER}-linux-x86_64.zip" ]; then
+    curl -LO https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VER}/protoc-${PROTOC_VER}-linux-x86_64.zip
+    unzip protoc-${PROTOC_VER}-linux-x86_64.zip -u -d $HOME/.local
+fi 
 go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@${PROTOC_GEN_DOC_VER}
-
 
 pushd api
 protoc --doc_out=../docs --doc_opt=markdown,protobom-ref.md sbom.proto 

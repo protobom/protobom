e1737da24f642e48f37a9a3f081c020580a93d9f
Jonathan Howard <jonathan.w.howard@lmco.com>
2024-10-07 11:48:09 -0500

    chore: add license header boilerplate
    Signed-off-by: Jonathan Howard <jonathan.w.howard@lmco.com>

--- api/generate.go
+++ api/generate.go
@@ -1,3 +1,8 @@
+// --------------------------------------------------------------
+// SPDX-FileCopyrightText: Copyright © 2024 The Protobom Authors
+// SPDX-FileType: SOURCE
+// SPDX-License-Identifier: Apache-2.0
+// --------------------------------------------------------------
 //go:build ignore
 // +build ignore
 

ba6bf940beb8eab81baef65e6d465bf19ce56ed7
Jonathan Howard <jonathan.w.howard@lmco.com>
2024-10-14 16:05:13 -0500

    ci: fetch tags on checkout
    Signed-off-by: Jonathan Howard <jonathan.w.howard@lmco.com>

--- hack/verify-proto.sh
+++ hack/verify-proto.sh
@@ -17,17 +17,7 @@ curl --fail --silent --show-error --location \
   --url "https://github.com/bufbuild/buf/releases/download/${BUF_VERSION}/buf-$(uname -s)-$(uname -m)" \
   --output .bin/buf && chmod +x "$_"
 
-# Install jq to parse JSON response
-if ! command -v jq &> /dev/null; then
-  (apt-get update && apt-get install --yes jq) &> /dev/null
-fi
-
-# Get latest tag from GitHub releases API
-releases_api=https://api.github.com/repos/protobom/protobom/releases/latest
-tag_name="$(curl --fail --silent --show-error --location --url "$releases_api" | jq --raw-output '.tag_name')"
-
 export PATH=${PWD}/.bin:${PATH}
-export VERSION=$tag_name
 
 make buf-format
 make buf-lint

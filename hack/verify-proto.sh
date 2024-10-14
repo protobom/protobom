87e12dd1fa6a794a43d6a2b98e23c75a2af37167
Jonathan Howard <jonathan.w.howard@lmco.com>
2024-10-14 16:17:48 -0500

    fix: check breaking changes against remote git URL
    Signed-off-by: Jonathan Howard <jonathan.w.howard@lmco.com>

--- hack/verify-proto.sh
+++ hack/verify-proto.sh
@@ -17,11 +17,18 @@ curl --fail --silent --show-error --location \
   --url "https://github.com/bufbuild/buf/releases/download/${BUF_VERSION}/buf-$(uname -s)-$(uname -m)" \
   --output .bin/buf && chmod +x "$_"
 
+# Install jq to parse JSON response
+if ! command -v jq &> /dev/null; then
+  (apt-get update && apt-get install --yes jq) &> /dev/null
+fi
+
+# Get latest tag from GitHub releases API
+releases_api=https://api.github.com/repos/protobom/protobom/releases/latest
+tag_name="$(curl --fail --silent --show-error --location --url "$releases_api" | jq --raw-output '.tag_name')"
+
 export PATH=${PWD}/.bin:${PATH}
 
-make buf-format
-make buf-lint
-make buf-generate
+VERSION=$tag_name make buf-format buf-lint buf-generate
 
 git diff --exit-code -- **/{*.pb,value_scanner}.go **/*.proto ||
   exit_with_msg "The protobuf definitions are not up to date. Check the docs and run make proto"

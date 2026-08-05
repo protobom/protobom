fccf330adcaed7b5ee30ce7b69a9fca5dd4a5d82
Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
2026-08-05 14:48:07 -0600

    Register the tag-value (un)serializer
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>

--- pkg/reader/reader.go
+++ pkg/reader/reader.go
@@ -51,6 +51,7 @@ func init() {
 	unserializers[formats.CDX16JSON] = drivers.NewCDX("1.6", formats.JSON)
 	unserializers[formats.CDX17JSON] = drivers.NewCDX("1.7", formats.JSON)
 	unserializers[formats.SPDX23JSON] = drivers.NewSPDX23()
+	unserializers[formats.SPDX23TV] = drivers.NewSPDX23TV()
 	regMtx.Unlock()
 }
 
